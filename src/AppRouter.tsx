f8a71f7454bcbf15ad1b02f7dbbbb93fad6dcfb9
PedroGj <pedrocsweb@gmail.com>
2025-11-24 18:10:53 -0300

    Create AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import { QuotePreviewPage } from './pages/QuotePreviewPage/QuotePreviewPage';
+
 export const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<QuotePreviewPage />}
+      />
+    </Routes>
+  );
 };
