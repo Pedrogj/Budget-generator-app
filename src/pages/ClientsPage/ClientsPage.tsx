bbac7e0299af1ee50d5f6a162d661a2d1c23a331
PedroGj <pedrocsweb@gmail.com>
2025-12-01 11:12:04 -0300

    Edit ClientPage component

--- src/pages/ClientsPage/ClientsPage.tsx
+++ src/pages/ClientsPage/ClientsPage.tsx
@@ -1,24 +1,65 @@
+import { useForm } from 'react-hook-form';
+import { useQuote } from '../../context/QuoteContext';
+import type { ClientInfo } from '../../types/types';
+
+interface ClientFormValues {
+  name: string;
+  rif: string;
+  address: string;
+}
+
 export const ClientsPage = () => {
+  const { clients, addClient } = useQuote();
+
+  const { register, handleSubmit, reset } = useForm<ClientFormValues>({
+    defaultValues: {
+      name: '',
+      rif: '',
+      address: '',
+    },
+  });
+
+  const onSubmit = (data: ClientFormValues) => {
+    addClient(data);
+    reset();
+  };
+
   return (
     <div className="page">
       <h1>Clientes</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="section">
           <label>
             <span>Nombre</span>
-            <input type="text" />
+            <input {...register('name', { required: true })} />
           </label>
           <label>
             <span>RIF</span>
-            <input type="text" />
+            <input {...register('rif', { required: true })} />
           </label>
           <label>
             <span>Dirección</span>
-            <input type="text" />
+            <input {...register('address', { required: true })} />
           </label>
-          <button>Agregar cliente</button>
+          <button type="submit">Agregar cliente</button>
         </div>
       </form>
+
+      <div className="section">
+        <h2>Listado de clientes</h2>
+        {clients.length === 0 ? (
+          <p>No hay clientes, empieza creando</p>
+        ) : (
+          <>
+            {clients.map((client: ClientInfo) => (
+              <div key={client.id}>
+                <strong>{client.name}</strong> - <p>{client.rif}</p>
+                <p>{client.address}</p>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
